566c1d27348990aeb68bdd919da4ef29765dbdb2
ABR293 <Varyag-93@mail.ru>
2019-11-21 15:27:30 +0300

    21.11.19/15.26 - удалось собрать стабильную сборку продолжаю делать UI

---
+++ src/Components/Calendar/Calendar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './Calendar.module.css'
+
+
+const Calendar = (props) => {
+    return (
+        <div className={style}>
+            Calendar
+        </div>
+    )
+};
+export default Calendar;
